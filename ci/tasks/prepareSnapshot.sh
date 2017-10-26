62438f9d2e135b0e6c09212676ce5cea1ba5519a
James Shingler <James.Shingler@CardinalHealth.com>
2017-10-25 21:45:36 -0400

    Export maven config

--- ci/tasks/prepareSnapshot.sh
+++ ci/tasks/prepareSnapshot.sh
@@ -34,7 +34,7 @@ baseVersion=$(echo ${baseVersion} | sed -e s/-.*//)
 
 #mvn dependency:copy -Dartifact=com.example:spring-music:1.2.2-SNAPSHOT -DoutputDirectory=./tmp -Drepository.url=http://10.210.231.83:8081/repository/odt-maven-snapshot/ -Drepository.username=admin -Drepository.password=admin123 -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 
-MAVEN_CONFIG="-s ./.mvn/settings.xml"
+export MAVEN_CONFIG="-s ./.mvn/settings.xml"
 echo "MAVEN_CONFIG = ${MAVEN_CONFIG}"
 ./mvnw dependency:copy -Dartifact=${groupID}:${artifactID}:${version} -DoutputDirectory=${destDir} -Drepository.url=${url} -Drepository.username=${username} -Drepository.password=${password} -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 

0358c5f36cfc104806b7a54b27ff71a01eecf25f
James Shingler <James.Shingler@CardinalHealth.com>
2017-10-25 20:20:23 -0400

    rename artifact

--- ci/tasks/prepareSnapshot.sh
+++ ci/tasks/prepareSnapshot.sh
@@ -20,6 +20,13 @@ version=${VERSION}
 destDir=${DESTDIR}
 username=${USERNAME}
 password=${PASSWORD}
+baseVersion=${version}
+# convert 1.0.0-20170328.031519-19 to 1.0.0-SNAPSHOT
+uniqueVersion=$(echo "$baseVersion" | grep -oE "[0-9]{8}\.[0-9]{6}-[0-9]{1,}" || true)
+if [ -n "$uniqueVersion" ]; then
+  baseVersion=$(echo ${baseVersion%-$uniqueVersion})
+fi
+baseVersion=$(echo ${baseVersion} | sed -e s/-.*//)
 
 # url=http://10.210.231.83:8081/repository/odt-maven-snapshot/
 # groupID=com.example
@@ -31,5 +38,6 @@ password=${PASSWORD}
 
 #mvn dependency:copy -Dartifact=com.example:spring-music:1.2.2-SNAPSHOT -DoutputDirectory=./tmp -Drepository.url=http://10.210.231.83:8081/repository/odt-maven-snapshot/ -Drepository.username=admin -Drepository.password=admin123 -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
 mvn dependency:copy -Dartifact=${groupID}:${artifactID}:${version} -DoutputDirectory=${destDir} -Drepository.url=${url} -Drepository.username=${username} -Drepository.password=${password} -Dmaven.wagon.http.ssl.insecure=true -Dmaven.wagon.http.ssl.allowall=true -Dmaven.wagon.http.ssl.ignore.validity.dates=true
-
+cd $destDir
+mv ${artifactID}-${version}.jar ${artifactID}-${baseVersion}.jar
 echo "PrepareSnapshot.sh -- Done"
